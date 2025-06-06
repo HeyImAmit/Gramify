99d1396519f3be793ef50aac870a8e363e67f11a
Subham Mohanty <subhamomm836@gmail.com>
2025-06-06 23:58:31 +0530

    Update gemini_model_loader.py

--- newml/gemini_model_loader.py
+++ newml/gemini_model_loader.py
@@ -1,5 +1,5 @@
 # gemini_model_loader.py
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCElD6bbZyjc-a8evE4R-QMG2Vn_vA-JFk")
-gemini_model = genai.GenerativeModel("gemini-1.5-pro")
+genai.configure(api_key="AIzaSyAQPwrCxu5baBotfkNGIL0frZsmYlfo6SM")
+gemini_model = genai.GenerativeModel("gemini-2.5-flash-preview-05-20")
