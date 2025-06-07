ee02c55d9ecdfafbc98fa8e6338cc619f74b15d1
Subham Mohanty <subhamomm836@gmail.com>
2025-06-07 20:00:27 +0530

    Update gemini_model_loader.py

--- ml/gemini_model_loader.py
+++ ml/gemini_model_loader.py
@@ -1,5 +1,13 @@
 # gemini_model_loader.py
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyAQPwrCxu5baBotfkNGIL0frZsmYlfo6SM")
+load_dotenv()  # Load variables from .env
+api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    raise ValueError("Missing GEMINI_API_KEY in environment")
+
+genai.configure(api_key=api_key)
 gemini_model = genai.GenerativeModel("gemini-2.5-flash-preview-05-20")
