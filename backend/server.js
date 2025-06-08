1dba817382e55973e39e356b9f9cf4af9c599c36
Subham Mohanty <subhamomm836@gmail.com>
2025-06-09 00:48:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const upload = multer({ dest: "uploads/" });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const FASTAPI_BASE_URL =
-  "http://34.55.80.43:8000";
+  "http://35.200.158.34:8000/";
 
 app.use(express.json());
 app.use(cors());
