d17b2e2685d6252d13fd7290a5abf6fb47247c83
Subham Mohanty <subhamomm836@gmail.com>
2025-06-08 12:29:14 +0530

    Update gemini_model_loader.py

--- ml/gemini_model_loader.py
+++ ml/gemini_model_loader.py
@@ -1,9 +1,7 @@
 # gemini_model_loader.py
 import os
-from dotenv import load_dotenv
 import google.generativeai as genai
 
-load_dotenv()  # Load variables from .env
 api_key = os.getenv("GEMINI_API_KEY")
 
 if not api_key:
@@ -11,3 +9,4 @@ if not api_key:
 
 genai.configure(api_key=api_key)
 gemini_model = genai.GenerativeModel("gemini-2.5-flash-preview-05-20")
+
